384f5447a2a5694114217af796a2da6650f7094b
MattiasV <mattias.vandecauter@gmail.com>
2021-09-20 14:24:22 +0200

    Changed name + added libraries

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from eBoiler import Ui_eBoiler
 
 from PyQt5 import QtWidgets
 
-class LoginWindow(QtWidgets.QMainWindow):
+class GUI_setup(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.ui = Ui_eBoiler()
         self.ui.setupUi(self)
 
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    w = LoginWindow()
+    w = GUI_setup()
     w.show()
     sys.exit(app.exec_())
