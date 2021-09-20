8d73113a79f23e4d3e25683b46db5471e70f2708
MattiasV <mattias.vandecauter@gmail.com>
2021-09-20 14:21:35 +0200

    main.py edited so we can see the UI

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import sys
 
+from eBoiler import Ui_eBoiler
 
 from PyQt5 import QtWidgets
 
+class LoginWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.ui = Ui_eBoiler()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    w = LoginWindow()
+    w.show()
+    sys.exit(app.exec_())
